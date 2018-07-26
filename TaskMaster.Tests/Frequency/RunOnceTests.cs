197ac8593dc79e9c65485779a0e930fc0d9f9c03
James Craig <none@none>
2018-07-26 15:26:04 -0400

    Added RunOnce for frequencies.

---
+++ TaskMaster.Tests/Frequency/RunOnceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TaskMaster.Frequency;
+using Xunit;
+
+namespace TaskMaster.Tests.Frequency
+{
+    public class RunOnceTests
+    {
+        [Fact]
+        public void CanRun()
+        {
+            var TestObject = new RunOnce();
+            Assert.False(TestObject.CanRun(new DateTime(1999, 1, 1), new DateTime(2000, 1, 1)));
+            Assert.False(TestObject.CanRun(new DateTime(2001, 1, 1), new DateTime(2000, 1, 1)));
+            Assert.False(TestObject.CanRun(new DateTime(2000, 1, 1), new DateTime(2000, 1, 1)));
+            Assert.False(TestObject.CanRun(new DateTime(2000, 1, 1, 1, 2, 0), new DateTime(2000, 1, 1, 1, 2, 0)));
+            Assert.False(TestObject.CanRun(new DateTime(1999, 1, 1, 1, 1, 0), new DateTime(2000, 1, 2)));
+            Assert.False(TestObject.CanRun(new DateTime(2000, 1, 2), new DateTime(2000, 1, 3)));
+            Assert.True(TestObject.CanRun(new DateTime(), new DateTime(2000, 1, 3)));
+        }
+    }
+}
---
+++ TaskMaster/Frequency/RunOnce.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TaskMaster.Interfaces;
+
+namespace TaskMaster.Frequency
+{
+    /// <summary>
+    /// Runs the task one time
+    /// </summary>
+    /// <seealso cref="IFrequency"/>
+    public class RunOnce : IFrequency
+    {
+        /// <summary>
+        /// Determines whether this instance can run based on the specified last run.
+        /// </summary>
+        /// <param name="lastRun">The last run.</param>
+        /// <param name="currentTime">The current time.</param>
+        /// <returns>True if it can, false otherwise</returns>
+        public bool CanRun(DateTime lastRun, DateTime currentTime)
+        {
+            return lastRun == DateTime.MinValue;
+        }
+    }
+}
